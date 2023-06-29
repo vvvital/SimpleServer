9a5da916c14636b5b614f6917540a1fac1b91664
Vitaliy Kovalenko <vvvital@i.ua>
2023-06-29 23:28:25 +0300

    move connect to separate thread

--- src/Connection.java
+++ src/Connection.java
@@ -4,31 +4,53 @@ import java.net.Socket;
 
 public class Connection {
 
+    private BufferedReader input;
+    private PrintWriter output;
+
     public void connection() throws IOException {
         while (true) {
             try (ServerSocket serverSocket = new ServerSocket(8084)) {
                 Socket socket = serverSocket.accept();
                 System.out.println("client connected");
-                BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                PrintWriter output = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
-                while (socket.isConnected()) {
-                    String header = input.readLine();
-                    if (header==null)break;
-                    String name = parseHeader(header);
-                    System.out.println(header);
-                    while (input.ready()) {
-                        System.out.println(input.readLine());
-                    }
-                    output.println("HTTP/1.1 200 OK");
-                    output.println("Content-Type: text/html; charset=utf-8");
-                    output.println();
-                    output.println("<p>Привет " + name + "!</p>");
-                    output.println("<a href='http://localhost:63342/SimpleServer/my1.html'>link</a>");
-                    output.flush();
-
-                }
+                input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                output = new PrintWriter(socket.getOutputStream());
+                SocketProcessor processor = new SocketProcessor(socket, input, output);
+                processor.run();
                 System.out.println("client disconnected");
             }
+        }
+    }
+
+    private class SocketProcessor implements Runnable {
+        public SocketProcessor(Socket socket, BufferedReader input, PrintWriter output) {
+            this.socket=socket;
+            this.input = input;
+            this.output = output;
+        }
+
+        private BufferedReader input;
+        private PrintWriter output;
+        private Socket socket;
+
+        @Override
+        public void run() {
+            try {
+                System.out.println("new thread started");
+                String header = input.readLine();
+                String name = parseHeader(header);
+                while (input.ready()) {
+                    System.out.println(input.readLine());
+                }
+                output.println("HTTP/1.1 200 OK");
+                output.println("Content-Type: text/html; charset=utf-8");
+                output.println();
+                output.println("<p>Привет " + name + "!</p>");
+                output.println("<a href='http://localhost:63342/SimpleServer/my1.html'>link</a>");
+                output.flush();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("thread stopped");
         }
     }
 
